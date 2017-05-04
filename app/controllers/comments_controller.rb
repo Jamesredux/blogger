94e39a329b2afbb699ccd5518fe6a791263280de
James McCann <jamesredux@gmail.com>
2017-05-04 18:56:02 +0700

    added delete comments feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,13 @@ def create
 	
 end
 
+def destroy
+	@article = Article.find(params[:article_id])
+	@comment = @article.comments.find(params[:id])
+	@comment.destroy
+	redirect_to article_path(@article)
+end
+
 def comment_params
 	params.require(:comment).permit(:author_name, :body)
 	
